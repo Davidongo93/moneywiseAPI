c196d666647bc50da5f553e861cd1941c5ae2ec1
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-16 22:09:44 -0500

    endpoint for get all users

--- src/main/java/com/soyhenry/moneywiseAPI/controller/UserController.java
+++ src/main/java/com/soyhenry/moneywiseAPI/controller/UserController.java
@@ -1,31 +1,37 @@
 package com.soyhenry.moneywiseAPI.controller;
 
 import com.soyhenry.moneywiseAPI.dao.dto.request.UserRequestDto;
-import com.soyhenry.moneywiseAPI.service.UserServiceImpl;
+import com.soyhenry.moneywiseAPI.dao.dto.response.UserResponseDto;
+import com.soyhenry.moneywiseAPI.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("api/v1/user")
 
 public class UserController {
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
-    public UserController(UserServiceImpl userServiceImpl) {
-        this.userServiceImpl = userServiceImpl;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
 
     public ResponseEntity<String> createUserHandler(@RequestBody UserRequestDto userRequestDto){
-        String response = userServiceImpl.createUser(userRequestDto);
-        return null;
+        String response = userService.createUser(userRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
     }
-   /* public User createUser(@RequestBody User user){
-    return null;
-    }*/
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> responses = userService.getAllUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
+
+
 }
