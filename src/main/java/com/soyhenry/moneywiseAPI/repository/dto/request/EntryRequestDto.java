e8860a88a47927838304fb4997cdf4968174bf22
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-18 17:27:39 -0500

    entryDTOs

---
+++ src/main/java/com/soyhenry/moneywiseAPI/repository/dto/request/EntryRequestDto.java
@@ -0,0 +1,71 @@
+package com.soyhenry.moneywiseAPI.repository.dto.request;
+
+import com.soyhenry.moneywiseAPI.model.EntryType;
+
+import java.util.Date;
+
+public class EntryRequestDto {
+    private int id;
+    private Date date;
+    private String description;
+    private double amount;
+    private EntryType type;  // Enum
+    private String category;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public EntryType getType() {
+        return type;
+    }
+
+    public void setType(EntryType type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
