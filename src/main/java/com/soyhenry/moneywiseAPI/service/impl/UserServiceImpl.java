9e8de080137d0f3618e1045b8d008b9def47e18d
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-21 18:16:57 -0500

    PUT response

--- src/main/java/com/soyhenry/moneywiseAPI/service/impl/UserServiceImpl.java
+++ src/main/java/com/soyhenry/moneywiseAPI/service/impl/UserServiceImpl.java
@@ -22,8 +22,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public String createUser(UserRequestDto userRequestDto) {
 
-    String response = "User created successfully";
-
     User user = mapDtoToUser(userRequestDto);
     Integer responseInserted = userRepository.insert(user);
     if (responseInserted.equals(0)){
@@ -34,7 +32,6 @@ public class UserServiceImpl implements UserService {
 
     private User mapDtoToUser(UserRequestDto userRequestDto) {
         User user = new User();
-        System.out.println("map dto to user REQUEST");
         user.setName(userRequestDto.getName());
         user.setEmail(userRequestDto.getEmail());
         user.setPass(userRequestDto.getPass());
@@ -43,17 +40,29 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserResponseDto> getAllUsers(){
         List<User> users = userRepository.getAll();
+        for (User user : users) {
+            System.out.println("User ID in getAllUsers: " + user.getId());
+        }
         return users.stream().
                 map(this::mapUsersToResponseDto).collect(Collectors.toList());
     }
 
     @Override
     public String updateUser(int id, UserRequestDto userRequestDto) {
-        String response = "User updated Succesfully";
+        String response = "User updated successfully";
         User user = mapDtoToUser(userRequestDto);
         Integer responsesUpdated = userRepository.updateUser(id, user);
+        if (responsesUpdated.equals(0)) {
+            System.out.println("User with ID " + id + " wasn't updated");
+            response = "Failed to update user. User with ID " + id + " not found.";
+        } else {
+            System.out.println("User with ID " + id + " updated successfully");
+            response += "\n User ID: " + id;
+        }
+
         return response;
     }
+
 
     @Override
     public void deleteUser(int id) throws DAOException {
@@ -63,8 +72,8 @@ public class UserServiceImpl implements UserService {
     private UserResponseDto mapUsersToResponseDto(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();
         userResponseDto.setId(user.getId());
-        System.out.println(user.getId());
-        userResponseDto.setName("user.getName()");
+        System.out.println("Mapped user ID: " + user.getId());
+        userResponseDto.setName(user.getName());
         userResponseDto.setEmail(user.getEmail());
 //      userResponseDto.setPass(user.getPass());
         return userResponseDto;
